a8318761bcbd97d374caa1614ddf9b215dc5848a
Loonyaw <127119093+Loonyaw@users.noreply.github.com>
2023-03-06 17:31:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, сливка! Введи /schedule для того, чтобы получить расписание на сегодня.")
 
 def help(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Список доступных команд:\\n/schedule")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Список доступных команд:\n/schedule")
 
 def schedule(update, context):
     now = datetime.datetime.now()
@@ -19,13 +19,13 @@ def schedule(update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, text="Ты бессмертн(-ый/-ая) что ли? Иди проспись")
         return
 
-    message = "Расписание на сегодня:\\n\\n"
+    message = "Расписание на сегодня:\n\n"
 
     for item in schedule:
         message += f"{item['time']}{item['name']}:\\n"
 
         for link in item['links']:
-            message += f"{link}\\n"
+            message += f"{link}\n"
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
@@ -35,103 +35,103 @@ if __name__ == '__main__':
     SCHEDULE = {
         'monday': [
             {
-                'time': '9:50-11:25\\n',
+                'time': '9:50-11:25\n',
                 'name': 'Дискретная математика (Лекция)',
                 'links': [
-                    'https://zoom.us/j/4951329852?pwd=eEtLNmRCUnpiY2ZUNGdFK1JqSkJWdz09\\n'
+                    'https://zoom.us/j/4951329852?pwd=eEtLNmRCUnpiY2ZUNGdFK1JqSkJWdz09\n'
                 ]
             },
             {
-                'time': '11:40-13:15\\n',
+                'time': '11:40-13:15\n',
                 'name': 'Операционные системы (Лекция)',
                 'links': [
                     'https://us02web.zoom.us/j/87688751716?pwd=MnlJa2pXN1dGeUgwMUprdjJwODJzQT09'
-                    '\\n\\nТерпения тебе!'
+                    '\n\nТерпения тебе!'
                 ]
             },
         ],
         'tuesday': [
             {
-                'time': '9:50-11:25\\n',
+                'time': '9:50-11:25\n',
                 'name': 'Алгоритмизация н/п (Лаб)',
                 'links': [
                     'https://zoom.us/j/98778795282?pwd=eEtLNmRCUnpiY2ZUNGdFK1JqSkJWdz09',
-                    'Пароль:893295\\n'
+                    'Пароль:893295\n'
                 ]
             },
             {
-                'time': '9:50-11:25\\n',
+                'time': '9:50-11:25\n',
                 'name': 'Алгоритмизация парная (Практ)',
                 'links': [
-                    'https://us02web.zoom.us/j/89424759693?pwd=eDFjMnJSL1lpVDFEdjVDSWhnRTdLZz09\\n',
+                    'https://us02web.zoom.us/j/89424759693?pwd=eDFjMnJSL1lpVDFEdjVDSWhnRTdLZz09\n',
                 ]
             },
             {
-                'time': '11:40-13:15\\n',
+                'time': '11:40-13:15\n',
                 'name': 'Дискретная математика (Практ)',
                 'links': [
                     'https://zoom.us/j/4951329852?pwd=eEtLNmRCUnpiY2ZUNGdFK1JqSkJWdz09',
-                    '\\nТерпения тебе!'
+                    '\nТерпения тебе!'
                 ]
             },
         ],
         'wednesday': [
             {
-                'time': '8:00-9:35\\n',
+                'time': '8:00-9:35\n',
                 'name': 'Английский язык (Практ)',
                 'links': [
                     'https://us05web.zoom.us/j/5884802236?pwd=eDVCOVlLdHJ4ZmVDaVVQTFhtMml1Zz09'
-                    '\\nПароль:11111111\\n'
+                    '\nПароль:11111111\n'
                 ]
             },
             {
-                'time': '9:50-11:25\\n',
+                'time': '9:50-11:25\n',
                 'name': 'Выш.мат (Лекция)',
                 'links': [
                     'https://us05web.zoom.us/j/6640162782?pwd=eDVCOVlLdHJ4ZmVDaVVQTFhtMml1Zz09'
-                    '\\nПароль:8NWy4H\\n'
+                    '\nПароль:8NWy4H\n'
                 ]
             },
             {
-                'time': '11:40-13:15\\n',
+                'time': '11:40-13:15\n',
                 'name': 'Прикладная физика (Лекция)',
                 'links': [
                     '*нет ссылки*'
-                    '\\n\\nТерпения тебе! (Особенно с Усовым)'
+                    '\n\nТерпения тебе! (Особенно с Усовым)'
                 ]
             }
         ],
         'thursday': [
             {
-                'time': '9:50-11:25\\n',
+                'time': '9:50-11:25\n',
                 'name': 'Выш.мат (Практ)',
                 'links': [
-                    'https://us05web.zoom.us/j/88252951933?pwd=MlZUdllYWE8rN3RVMTV5SmpjT2V6QT09\\n'
+                    'https://us05web.zoom.us/j/88252951933?pwd=MlZUdllYWE8rN3RVMTV5SmpjT2V6QT09\n'
                 ]
             },
             {
-                'time': '11:40-13:15\\n',
+                'time': '11:40-13:15\n',
                 'name': 'Операционные системы (Лаб)',
                 'links': [
-                    'https://us02web.zoom.us/j/87688751716?pwd=MnlJa2pXN1dGeUgwMUprdjJwODJzQT09\\n'
-                    '\\nТерпения тебе! (Слава богу Желиба)'
+                    'https://us02web.zoom.us/j/87688751716?pwd=MnlJa2pXN1dGeUgwMUprdjJwODJzQT09\n'
+                    '\nТерпения тебе! (Слава богу Желиба)'
                 ]
             }
         ],
         'friday': [
             {
-                'time': '9:50-11:25\\n',
+                'time': '9:50-11:25\n',
                 'name': 'Алгоритмизация (Лекция)',
                 'links': [
-                    'https://us02web.zoom.us/j/89424759693?pwd=eDFjMnJSL1lpVDFEdjVDSWhnRTdLZz09\\n'
+                    'https://us02web.zoom.us/j/89424759693?pwd=eDFjMnJSL1lpVDFEdjVDSWhnRTdLZz09\n'
                 ]
             },
             {
-                'time': '11:40-13:15\\n',
+                'time': '11:40-13:15\n',
                 'name': 'Прикладная физика (Лаб)',
                 'links': [
-                    '*нет ссылки*\\n'
-                    '\\nТерпения тебе!'
+                    '*нет ссылки*\n'
+                    '\nТерпения тебе!'
                 ]
             }
         ]
