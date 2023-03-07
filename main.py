eaf72a71e418995ca62762a98b486411a815f475
Loonyaw <127119093+Loonyaw@users.noreply.github.com>
2023-03-07 15:10:22 +0200

    main

--- main.py
+++ main.py
@@ -10,21 +10,26 @@ bot = telebot.TeleBot('5844782786:AAGqpYHZMmRZ3sfWdoGioA8FODBweFEG-eA')
 def start(message):
     button = telebot.types.KeyboardButton('/Расписание')
     keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True).add(button)
-    bot.send_message(chat_id=message.chat.id, text='Привет, сливка!', reply_markup=keyboard)
+    bot.send_message(chat_id=message.chat.id, text='Привет, сливка! Нажми на кнопку, чтобы получить расписание!', reply_markup=keyboard)
 
 @bot.message_handler(commands=['Расписание'])
 def schedule(message):
     now = datetime.datetime.now()
     day = now.strftime('%A').lower()
 
+    with open('day_names_ru.json', 'r', encoding='utf-8') as f:
+        day_names = json.load(f)
+
+    day_ru = day_names.get(day, day)
+
     with open('schedule.json', 'r', encoding='utf-8') as f:
         schedule = json.load(f).get(day)
-    
+
     if not schedule:
         bot.send_message(chat_id=message.chat.id, text='Ты бессмертн(-ый/-ая) что ли? Иди проспись')
         return
 
-    message_text = 'Расписание на сегодня:\n\n'
+    message_text = f"Расписание на {day_ru.capitalize()}:\n\n"
 
     for item in schedule:
         message_text += '{}{}:\n'.format(item['time'], item['name'])
